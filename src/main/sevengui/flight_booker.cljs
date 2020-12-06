fcebef48f9cad9f39d2eff1f679a97662a5d0968
Jaime Sangcap <jaime.sangcap@gmail.com>
2020-12-06 23:48:06 +0400

    finish flight booker

--- src/main/sevengui/flight_booker.cljs
+++ src/main/sevengui/flight_booker.cljs
@@ -8,28 +8,62 @@
     (str/split #"T")
     (first)))
 
+(defn flight-date [flight]
+  (str
+    (:start-date flight)
+    (when (= (:selected-option flight) "return")
+      (str " to " (:return-date flight)))))
+
+;; Keeping the date validation simple for now as I'm not using any library
+(defn valid-date? [date-string]
+  (not (nil? (re-matches #"\d{4}-\d{2}-\d{2}" date-string))))
+
+(defn valid-date-range? [start end]
+  (and
+    (not (nil? start))
+    (not (nil? end))
+    (>= (js/Date. end) (js/Date. start))))
+
+(defn valid-booking? [{:keys [start-date return-date selected-option]}]
+  (and (valid-date? start-date)
+       (or (= selected-option "one-way")
+           (and (valid-date? return-date)
+                (valid-date-range? start-date return-date)))))
+
 
 (defn flight-booker [{:keys [default-date]
-                      :or {default-date (js/Date.)}}]
-  (let [state (r/atom {:selected-option :one-way
+                      :or {default-date (format-date (js/Date.))}}]
+  (let [state (r/atom {:selected-option "one-way"
                        :start-date default-date
-                       :return-date default-date})]
+                       :return-date default-date})
+        handle-submit (fn [e]
+                        (.preventDefault e)
+                        (swap! state assoc
+                          :success-message (str "You have booked a " (:selected-option @state) " flight on " (flight-date @state))))]
     (fn []
-      [:div
+      [:form {:on-submit handle-submit}
+       (when (:success-message @state) [:div {:testid "success-message"} (:success-message @state)])
        [:div
-        [:select {:on-change #(swap! state assoc
-                                :selected-option (.. % -target -value))}
-         [:option {:value :one-way} "one-way flight"]
-         [:option {:value :return} "return flight"]]]
+        [:select {:on-change #(swap! state assoc :selected-option (.. % -target -value))}
+         [:option {:value "one-way"} "one-way flight"]
+         [:option {:value "return"} "return flight"]]]
        [:div
         [:input {:type "text"
+                 :style {:background-color (when-not (valid-date? (:start-date @state)) "red")}
                  :aria-label :start-date
                  :name :start-date
-                 :value (format-date (:start-date @state))}]]
+                 :value (:start-date @state)
+                 :on-change #(swap! state assoc :start-date (.. % -target -value))}]]
        [:div
         [:input {:type "text"
+                 :style {:background-color (when (and (not (valid-date? (:return-date @state)))
+                                                      (= "return" (:selected-option @state)))
+                                             "red")}
                  :aria-label :return-date
                  :name :return-date
-                 :value (format-date (:return-date @state))
-                 :disabled (not= :return (:selected-option @state))}]]
-       [:button {:type "submit"} "book"]])))
+                 :value (:return-date @state)
+                 :on-change #(swap! state assoc :return-date (.. % -target -value))
+                 :disabled (not= "return" (:selected-option @state))}]]
+       [:button {:type "submit"
+                 :disabled (not (valid-booking? @state))}
+        "book"]])))
--- src/test/sevengui/flight_booker_test.cljs
+++ src/test/sevengui/flight_booker_test.cljs
@@ -1,6 +1,8 @@
 (ns sevengui.flight-booker-test
   (:require
     [cljs.test :refer [deftest testing is]]
+    ["@testing-library/react" :as rtl]
+    [reagent.core :as r]
     [sevengui.test-utils :refer [with-component]]
     [sevengui.flight-booker :refer [flight-booker]]))
 
@@ -8,7 +10,7 @@
   (testing "should render flight-booker in correct state"
     (let [default-date-string "2021-02-17"]
       (with-component
-        [flight-booker {:default-date (js/Date. default-date-string)}]
+        [flight-booker {:default-date default-date-string}]
         (fn [view]
           (let [start-date-input (.getByRole view "textbox" #js {:name "start-date"})
                 return-date-input (.getByRole view "textbox" #js {:name "return-date"})
@@ -17,14 +19,113 @@
             (is (= default-date-string (.-value start-date-input)))
             (is (= default-date-string (.-value return-date-input)))
             (is (= 2 (count options)))
-            (is (= (name :one-way) (.-value (first options))))
+            (is (= "one-way" (.-value (first options))))
             (is (= true (.-selected (first options))))
-            (is (= (name :return) (.-value (nth options 1))))
+            (is (= "return" (.-value (nth options 1))))
             (is (= true (.-disabled return-date-input)))
             (is (= false (.-disabled button))))))))
+
+  (testing "should display a message when book button has been clicked for one-way flight"
+    (with-component
+      [flight-booker]
+      (fn [view]
+        (let [start-date-input (.getByRole view "textbox" #js {:name "start-date"})
+              button (.getByRole view "button")]
+          (.change rtl/fireEvent start-date-input (clj->js {:target {:value "2020-12-24"}}))
+          (r/flush)
+          (.click rtl/fireEvent button)
+          (r/flush)
+          (.getByText view "You have booked a one-way flight on 2020-12-24")))))
+
+  (testing "should display a message when book button has been clicked for return flight"
+    (with-component
+      [flight-booker]
+      (fn [view]
+        (let [select (.getByRole view "combobox")
+              start-date-input (.getByRole view "textbox" #js {:name "start-date"})
+              return-date-input (.getByRole view "textbox" #js {:name "return-date"})
+              button (.getByRole view "button")]
+          (.change rtl/fireEvent select (clj->js {:target {:value "return"}}))
+          (.change rtl/fireEvent start-date-input (clj->js {:target {:value "2020-12-23"}}))
+          (.change rtl/fireEvent return-date-input (clj->js {:target {:value "2021-01-03"}}))
+          (r/flush)
+          (.click rtl/fireEvent button)
+          (r/flush)
+          (.getByText view "You have booked a return flight on 2020-12-23 to 2021-01-03")))))
 
   (testing "should enable return date textbox when return flight option is selected"
     (with-component
       [flight-booker]
       (fn [view]
-        ()))))
+        (let [select (.getByRole view "combobox")
+              return-date-input (.getByRole view "textbox" #js {:name "return-date"})]
+          (is (= true (.-disabled return-date-input)))
+          (.change rtl/fireEvent select (clj->js {:target {:value "return"}}))
+          (r/flush)
+          (is (= false (.-disabled return-date-input)))
+          (.change rtl/fireEvent select (clj->js {:target {:value "one-way"}}))
+          (r/flush)
+          (is (= true (.-disabled return-date-input)))))))
+
+  (testing "should disable book button and change background to red when enabled date field have invalid value for one-way flight"
+    (with-component
+      [flight-booker]
+      (fn [view]
+        (let [start-date-input (.getByRole view "textbox" #js {:name "start-date"})
+              button (.getByRole view "button")]
+          (is (= false (.-disabled button)))
+          (.change rtl/fireEvent start-date-input (clj->js {:target {:value "invalid-date"}}))
+          (r/flush)
+          (is (= "red" (.. start-date-input -style -backgroundColor)))
+          (is (= true (.-disabled button)))
+          ;; ensure proper state after changed to valid date
+          (.change rtl/fireEvent start-date-input (clj->js {:target {:value "2020-12-25"}}))
+          (r/flush)
+          (is (= "" (.. start-date-input -style -backgroundColor)))
+          (is (= false (.-disabled button)))))))
+
+  (testing "should disable book button and change background to red when enabled date field have invalid value for return flight"
+    (with-component
+      [flight-booker]
+      (fn [view]
+        (let [select (.getByRole view "combobox")
+              start-date-input (.getByRole view "textbox" #js {:name "start-date"})
+              return-date-input (.getByRole view "textbox" #js {:name "return-date"})
+              button (.getByRole view "button")]
+          (is (= false (.-disabled button)))
+          (.change rtl/fireEvent select (clj->js {:target {:value "return"}}))
+          (r/flush)
+          (.change rtl/fireEvent start-date-input (clj->js {:target {:value "invalid-date"}}))
+          (.change rtl/fireEvent return-date-input (clj->js {:target {:value "invalid-date"}}))
+          (r/flush)
+          (is (= "red" (.. start-date-input -style -backgroundColor)))
+          (is (= "red" (.. return-date-input -style -backgroundColor)))
+          (is (= true (.-disabled button)))
+          ;; ensure proper state after changing to valid date
+          (.change rtl/fireEvent start-date-input (clj->js {:target {:value "2020-12-25"}}))
+          (.change rtl/fireEvent return-date-input (clj->js {:target {:value "2020-12-25"}}))
+          (r/flush)
+          (is (= "" (.. start-date-input -style -backgroundColor)))
+          (is (= "" (.. return-date-input -style -backgroundColor)))
+          (is (= false (.-disabled button)))))))
+
+  (testing "should disable book button when return-date is before the start-date"
+    (with-component
+      [flight-booker]
+      (fn [view]
+        (let [select (.getByRole view "combobox")
+              start-date-input (.getByRole view "textbox" #js {:name "start-date"})
+              return-date-input (.getByRole view "textbox" #js {:name "return-date"})
+              button (.getByRole view "button")]
+          (.change rtl/fireEvent select (clj->js {:target {:value "return"}}))
+          (.change rtl/fireEvent start-date-input (clj->js {:target {:value "2020-12-04"}}))
+          (.change rtl/fireEvent return-date-input (clj->js {:target {:value "2020-12-05"}}))
+          (r/flush)
+          (is (= false (.-disabled button)))
+          (.change rtl/fireEvent return-date-input (clj->js {:target {:value "2020-12-03"}}))
+          (r/flush)
+          (is (= true (.-disabled button)))
+          (.change rtl/fireEvent start-date-input (clj->js {:target {:value "2020-12-01"}}))
+          (r/flush)
+          (is (= false (.-disabled button))))))))
+
